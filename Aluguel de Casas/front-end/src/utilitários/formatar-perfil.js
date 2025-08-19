e5a155110ca5f0b35991885831556d0207f2f1ef
Issaka Diaw <dsissaka@gmail.com>
2025-08-19 16:07:07 -0300

    Delete Aluguel de Casas directory

--- Aluguel de Casas/front-end/src/utilitários/formatar-perfil.js
+++
@@ -1,7 +0,0 @@
- export default function formatarPerfil(perfil) {
-  switch(perfil) {
-    case "inquilino": return "Inquilino";
-    case "morador-adicional": return "MoradorAdicional";
-    default: return;
-  }
- };

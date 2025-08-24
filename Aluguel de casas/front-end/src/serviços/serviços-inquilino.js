f16fb7cef844a2884c3bc8480653ea2e3172d8c6
Issaka Diaw <dsissaka@gmail.com>
2025-08-24 11:47:41 -0300

    Delete Aluguel de casas directory

--- Aluguel de casas/front-end/src/serviços/serviços-inquilino.js
+++
@@ -1,4 +0,0 @@
- import servidor from "./servidor";
- export function serviçoCadastrarInquilino(inquilino)
-   { return servidor.post("/inquilinos", inquilino); };
- export function serviçoBuscarInquilino(cpf) { return servidor.get(`/inquilinos/${cpf}`); };
